8658527cfb750ac5efa1a0ef9143fe9d3aee282f
wigilabsDev5 <jmunoz@wigilabs.com>
2022-10-24 12:06:19 -0500

    Change test pipeline2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Proyecto CI/CD nuevo prueba'));
+app.get('/', (req, res) => res.send('Proyecto CI/CD '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
